df43598e4382d7de29a62b5f54c779156307d2d9
ashir <ashirza@gmail.com>
2021-03-11 17:17:52 +0000

    updated manhattanDistance to take coords

--- src/main/java/RuntimeTerror2021/UtilMethods.java
+++ src/main/java/RuntimeTerror2021/UtilMethods.java
@@ -3,11 +3,9 @@ package RuntimeTerror2021;
 import java.lang.Math;
 
 public class UtilMethods {
-    public static int getManhattanDistance(int coordA, int coordB, int coordC, int coordD) {
-        // coordA = x coordinate of point A
-        // coordB = y coordinate of point A
-        // coordC = x coordinate of point B
-        // coordD = y coordinate of point B
-        return Math.abs(coordA - coordC)  + Math.abs(coordB - coordD);
+    public static int getManhattanDistance(Coord a, Coord b) {
+        return Math.abs(a.x_col - b.x_col) + Math.abs(a.y_row - b.y_row);
     }
 }
+
+
