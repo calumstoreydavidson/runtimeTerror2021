ed226c56d59472da667e658b7f56a32a4bfe6284
calumstoreydavidson <calumsd@DESKTOP-CV3IVVG.localdomain>
2021-03-11 17:15:11 +0000

    put coord class in buildings

--- src/main/java/RuntimeTerror2021/Building.java
+++ src/main/java/RuntimeTerror2021/Building.java
@@ -3,28 +3,22 @@ package RuntimeTerror2021;
 import java.util.ArrayList;
 
 public class Building {
-    private int x_coord;
-    private int y_coord;
+    private Coord coord;
     private int latencyWeight;
     private int connectionSpeedWeight;
 
     public Building(String x_coord, String y_coord, String latencyWeight, String connectionSpeedWeight) {
-        this.x_coord = Integer.parseInt(x_coord);
-        this.y_coord = Integer.parseInt(y_coord);
+        this.coord = new Coord(Integer.parseInt(y_coord, Integer.parseInt(x_coord));
         this.latencyWeight = Integer.parseInt(latencyWeight);
         this.connectionSpeedWeight = Integer.parseInt(connectionSpeedWeight);
     }
 
-    public ArrayList<> getRange(){
-
-    }
-
     public int getX_coord() {
-        return x_coord;
+        return coord.getX_col();
     }
 
     public int getY_coord() {
-        return y_coord;
+        return coord.getY_row();
     }
 
     public int getLatencyWeight() {
