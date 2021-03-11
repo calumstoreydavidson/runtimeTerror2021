98d0420bbfa38cb75df541df66f023328911b03f
ashir <ashirza@gmail.com>
2021-03-11 17:46:48 +0000

    made method for getting coordinates that an antenna can be placed in around a building given some coords and a range

--- src/main/java/RuntimeTerror2021/UtilMethods.java
+++ src/main/java/RuntimeTerror2021/UtilMethods.java
@@ -6,12 +6,6 @@ public class UtilMethods {
     public static int getManhattanDistance(Coord a, Coord b) {
         return Math.abs(a.x_col - b.x_col) + Math.abs(a.y_row - b.y_row);
     }
-
-    public static ArrayList<Coords> getBuildingAntennaCoords(Coords building, int range) {
-        ArrayList<Coords> coordsList = new ArrayList<Coords>();
-
-        return coordsList;
-    }
 }
 
 
